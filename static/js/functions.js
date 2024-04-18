5208c46ebd2b2c19cdea3838ab6950739f7afe1d
Kenway Tennant <kengtennant@gmail.com>
2024-04-17 20:46:25 -0700

    Finishing Touches

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,11 +1,11 @@
 function getColor(crime) {
     //  Function to determine the color of the marker based on the depth of the earthquake
-    return crime > 60000 ? '#800026' :
-        crime > 55000 ? '#BD0026' :
-            crime > 50000 ? '#E31A1C' :
-                crime > 45000 ? '#FC4E2A' :
-                    crime > 40000 ? '#FD8D3C' :
-                        crime > 35000 ? '#FEB24C' :
+    return crime > 45000 ? '#800026' :
+        crime > 42000 ? '#BD0026' :
+            crime > 39000 ? '#E31A1C' :
+                crime > 35000 ? '#FC4E2A' :
+                    crime > 30000 ? '#FD8D3C' :
+                        crime > 25000 ? '#FEB24C' :
                             '#FFEDA0';
 }
 
@@ -22,7 +22,7 @@ function GetMarkers(data) {
     // Loop through the crimeArray and create one circle marker for each crime
     for (let i = 0; i < crimeArray.length; i++) {
         let crime = crimeArray[i];
-        let markerRadius = crime.Count * .00045;
+        let markerRadius = crime.Count * .00055;
 
         L.circleMarker(crime.Coordinates, {
             radius: markerRadius, // Use the Count to determine the radius
